36c4b84518cd78498a55cdc9399929e351245b03
francois-niang1 <francois.niang@laplateforme.io>
2022-03-14 10:37:58 +0100

    job 4 done

--- jour01/job04/index.php
+++ jour01/job04/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src='script.js'> </script>
-    <title>Jour 1 job 3</title>
+    <title>Jour 1 job 4</title>
 </head>
 <body>
     <main>
---
+++ jour01/job04/script.js
@@ -0,0 +1,10 @@
+function bisextile(année){
+    if (année % 4 == 0){
+        alert('TRUE');
+    }
+    else{
+        alert('FALSE');
+    }
+}
+
+bisextile(2017);
