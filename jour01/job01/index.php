f89c9db3062b640782583212c22ff920d56908e2
francois-niang1 <francois.niang@laplateforme.io>
2022-03-14 10:27:33 +0100

    job2 done

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" src='script.js'> </script>
+    <script>
+        alert('Hello Javascript!');
+    </script>
     <title>Jour 1 job 1</title>
 </head>
 <body>
