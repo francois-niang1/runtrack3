3032e78b14b513146537be3710610b0aeed350a2
francois-niang1 <francois.niang@laplateforme.io>
2022-03-17 11:18:42 +0100

    job01 done

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -8,6 +8,7 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <button>Click Me</button>
+        <p></p>
 </body>
 </html>
--- jour04/job01/script.js
+++ jour04/job01/script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
+    let btn = document.querySelector('button')
+    let txt = document.querySelector('p')
+
+    btn.addEventListener('click', function (){
+        fetch('expression.txt')
+        .then((response) => response.text())
+        .then((response) => txt.innerHTML = response)
+        .catch((error) => console.log(error))
+    })
 
 })
