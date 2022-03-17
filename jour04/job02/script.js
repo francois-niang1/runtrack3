4c2f1a6771bb15fde746bfeea8ec08b821923d99
francois-niang1 <francois.niang@laplateforme.io>
2022-03-17 13:22:00 +0100

    job02 done

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    
+    const strJSON = '{"name": "La Plateforme_", "address": "8 rue d\'hozier", "city": "Marseille", "nb_staff": "11", "creation": "2019"}';
+    const obj = JSON.parse(strJSON);
+    
 
-
+    function jsonValueKey(strJSON, key) {
+        console.log (obj[key])
+    }
+    jsonValueKey(strJSON, 'creation')
 })
