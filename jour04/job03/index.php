4a2d700f8e3ade7eb96c48223216feb2d3742750
francois-niang1 <francois.niang@laplateforme.io>
2022-03-17 14:17:04 +0100

    affichage type

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -8,29 +8,16 @@
     <title>Document</title>
 </head>
 <body>
-    <input type="text" placeholder="id">
-    <input type="text" placeholder="nom">
-    <select name="typer" id="type">
-        <option value="Normal">Normal</option>
-        <option value="Fire">Fire</option>
-        <option value="Water">Water</option>
-        <option value="Grass">Grass</option>
-        <option value="Electric">Electric</option>
-        <option value="Ice">Ice</option>
-        <option value="Fighting">Fighting</option>
-        <option value="Poison">Poison</option>
-        <option value="Ground">Ground</option>
-        <option value="Flying">Flying</option>
-        <option value="Psychic">Psychic</option>
-        <option value="Bug">Bug</option>
-        <option value="Rock">Rock</option>
-        <option value="Ghost">Ghost</option>
-        <option value="Dark">Dark</option>
-        <option value="Dragon">Dragon</option>
-        <option value="Steel">Steel</option>
-    </select>
-    <button type='filter'> Filtrer </button>
-
+    <form action="">
+        <input id='id' type="text" placeholder="id">
+        <input id='name' type="text" placeholder="nom">
+        
+        <select name="type" id="type">
+    
+        </select>
+    
+        <button type='filter'> Filtrer </button>
+    </form>
     <ul>
 
     </ul>
--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
+    let id = document.getElementById('id').value
+    let name = document.getElementById('name').value
+    let btn = document.getElementById('filter')
+    let select = document.querySelector('select')
+
+
+    // btn.addEventListener('click', function(){
+    //     alert(id)
+    // })
+    
+    fetch ('pokemon.json')
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response)
+        for (let i = 0; i < response.length ; i++ ){
+            let NewOpt = document.createElement('option')
+            NewOpt.innerHTML = response[i].type
+            select.append(NewOpt)
+        }
+    })
+    .catch((error) => console.log(error))
 
 })
