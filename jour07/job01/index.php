1b5c5c5d1eb9e1b2b6be3b88756e171aaeaa97bc
francois-niang1 <francois.niang@laplateforme.io>
2022-03-22 13:55:54 +0100

    job01 done

--- jour07/job01/index.php
+++ jour07/job01/index.php
@@ -9,28 +9,21 @@
     <title>Document</title>
 </head>
 <body>
-    <header>
+<header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">LPTF</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Accueil </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Units</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Job</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Skill</a>
-                    </li>
-                </ul>
+            <div class="container-fluid">
+                <a href="#" class="navbar-brand">LPTF</a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav">
+                        <a href="#" class="nav-item nav-link active">Accueil</a>
+                        <a href="#" class="nav-item nav-link">Units</a>
+                        <a href="#" class="nav-item nav-link">Job</a>
+                        <a href="#" class="nav-item nav-link disabled" tabindex="-1">Skills</a>
+                    </div>
+                </div>
             </div>
         </nav>
     </header>
